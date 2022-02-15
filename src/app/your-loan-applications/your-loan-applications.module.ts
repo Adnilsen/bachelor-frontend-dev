eaa35a2ed5de9133aa79d663504d4f5c8e387e0d
adnilsen <adrian.bn@hotmail.com>
2022-02-15 13:53:47 +0100

    testing-angular-materials

--- src/app/your-loan-applications/your-loan-applications.module.ts
+++ src/app/your-loan-applications/your-loan-applications.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { YourLoanApplicationsRoutingModule } from './your-loan-applications-routing.module';
 import { YourLoanApplicationsComponent } from './your-loan-applications.component';
-
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  declarations: [
-    YourLoanApplicationsComponent
-  ],
-  imports: [
-    CommonModule,
-    YourLoanApplicationsRoutingModule
-  ]
+  declarations: [YourLoanApplicationsComponent],
+  imports: [CommonModule, YourLoanApplicationsRoutingModule, MatSliderModule],
 })
-export class YourLoanApplicationsModule { }
+export class YourLoanApplicationsModule {}
