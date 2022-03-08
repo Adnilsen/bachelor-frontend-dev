88a1c967223516d838b70f7f1ba9eec083acac6c
martkvam <56063587+martkvam@users.noreply.github.com>
2022-03-08 13:48:04 +0100

    Reformattering fra prettier

--- src/app/core/broker/broker.service.ts
+++ src/app/core/broker/broker.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from "@angular/common/http";
-import {Broker} from "../../shared/interfaces/broker.interface";
+import { HttpClient } from '@angular/common/http';
+import { Broker } from '../../shared/interfaces/broker.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BrokerService {
-  constructor(private httpClient : HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
   getBrokers() {
-    return this.httpClient.get<Broker[]>("http://localhost:8080/brokers");
+    return this.httpClient.get<Broker[]>('http://localhost:8080/brokers');
   }
 }
