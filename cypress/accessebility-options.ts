907f25811e28ce612fd07b8c27faf13d94821556
Adnilsen <45078921+Adnilsen@users.noreply.github.com>
2022-03-15 15:20:03 +0100

    Feature/t55 Cypress tests (#14)
    * Cypress tests

---
+++ cypress/accessebility-options.ts
@@ -0,0 +1,8 @@
+import { RunOptions } from 'axe-core';
+
+export const A11Y_OPTIONS: RunOptions = {
+  runOnly: {
+    type: undefined,
+    values: ['wcag21aa', 'wcag21a', 'wcag2a', 'wcag2aa'],
+  },
+};
